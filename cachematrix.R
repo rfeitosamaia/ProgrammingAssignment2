87d705c174b05870cb1d61d3e54fd5befa5072f5
rfeitosamaia <rfeitosamaia@hotmail.com>
2017-04-02 18:04:43 -0300

    Update cachematrix.R
    Solving the Project Assignment 2 of Coursera Data Science Specitalization.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix returns a list containing 4 functions to set and get a matrix and its
+## inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  
+  get <- function() {
+    return(x)
+  }
+  
+  setinverse <- function(inverse) {
+    i <<- inverse
+  }
+  
+  getinverse <- function() {
+    return(i)
+  }
+  
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function calculates the inverse of a matrix and set it to the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+  
 }
